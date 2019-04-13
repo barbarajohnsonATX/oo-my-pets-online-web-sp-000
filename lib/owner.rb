09e953a4742518b1831d7b970b95d8c1b4bcf5c0
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 19:37:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
   # code goes here
+  
+  attr_reader :species 
+  attr_accessor :name 
+  
+  @@all = []
+  @pets = {}
+  
+  def initialize(species)
+    @species = species 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.length 
+  end 
+  
+  def self.reset_all 
+    @@all = []
+  end 
+  
+  def say_species
+    @species 
+  end 
+  
+  def pets 
+    @pets
+  
 end
